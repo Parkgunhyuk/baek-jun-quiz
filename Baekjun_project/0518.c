9411a0e727ff00f44fa25ad2d44b60b100bcce73
Parkgunhyuk <ekflalrkwk@naver.com>
2023-05-18 19:47:57 +0900

    0518 quiz

---
+++ Baekjun_project/0518.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int a, b; //10952 번 문제 
+//		while (1)
+//		{
+//			scanf("%d %d", &a, &b);
+//			if (a + b == 0)
+//				break;
+//			printf("%d\n", a + b);
+//		}
+//}
+
+//int main() {
+//	int a, b; //10951 번 문제
+//	while (scanf("%d %d", &a, &b) != EOF) {
+//		printf("%d\n", a + b);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, v; //10870 번 문제 
+//	a = 0, v = 0;
+//	int count = 0;
+//	int arr[100];
+//
+//	scanf("%d",&a);
+//
+//	for (int i = 0; i < a; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	
+//	scanf("%d", &v);
+//
+//	for (int i = 0; i < a; i++)
+//	{
+//		if (arr[i] == v)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d", count);
+//}
+
